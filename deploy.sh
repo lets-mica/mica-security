7fd81308e081afb0b25c56ce1a3fe0b6dcc4f789
程式人生 <9943426+icodelife@users.noreply.github.com>
2021-05-14 22:50:37 +0800

    :bug: 修复部署脚本

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,8 @@
 #代码打包
 mvn clean -U package -DskipTests > /dev/null
 
-#重命名jar
-mv ./target/dream-security-*.jar ./target/dream-security.jar
-
 #上传代码
-scp ./target/dream-security.jar tx1:/home/java/
+scp ./target/mica-security.jar tx1:/home/java/
 scp ./script/start.sh tx1:/home/java/
 
 #远程执行命令
