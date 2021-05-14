7c20af43ace2e6808dd80c9ce5df452de8ada896
程式人生 <9943426+icodelife@users.noreply.github.com>
2021-05-14 22:46:57 +0800

    :bug: 修复部署脚本

--- script/start.sh
+++ script/start.sh
@@ -3,7 +3,7 @@
 cd /home/java/
 
 #杀死老进程
-kill -9 `ps aux|grep dream-security |grep -v grep |grep java |awk '{print $2}'`
+kill -9 `ps aux|grep mica-security |grep -v grep |grep java |awk '{print $2}'`
 
 #启动
-nohup java -Djava.security.egd=file:/dev/./urandom -Xmx400M -Xms400M -jar dream-security.jar --spring.profiles.active=prod &
+nohup java -Djava.security.egd=file:/dev/./urandom -Xmx400M -Xms400M -jar mica-security.jar --spring.profiles.active=prod &
